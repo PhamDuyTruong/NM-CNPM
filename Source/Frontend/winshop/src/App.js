7947b65911705f0734fd4894496dd21a0d47e9bb
20120230 <duytruong5437@gmail.com>
2022-11-17 15:01:52 +0700

    set up frontend winshop

---
+++ Source/Frontend/winshop/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function App() {
+  return (
+    <div className="App">
+      Hello React
+    </div>
+  );
+}
+
+export default App;
---
+++ Source/Frontend/winshop/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// Set up Store
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+// Sử dụng middleware
+
+import rootReducer from './reducers';
+
+//const middleware = applyMiddleware(thunk);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Tạo store từ reducer
+const store = createStore(
+  rootReducer, 
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
