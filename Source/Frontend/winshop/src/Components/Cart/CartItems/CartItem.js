ccc2c9747bb7156ee1beeb8e86f26c73275689a4
20120230 <duytruong5437@gmail.com>
2022-12-26 16:20:55 +0700

    fix error about cart

--- Source/Frontend/winshop/src/Components/Cart/CartItems/CartItem.js
+++ Source/Frontend/winshop/src/Components/Cart/CartItems/CartItem.js
@@ -1,12 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./CartItem.scss";
 import { Button } from "@material-ui/core";
+import {useDispatch} from 'react-redux'
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import HandleImage from '../../../utils/HandleImage';
+import {addToCart} from '../../../actions/CartAction';
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
 
 function CartItem({productCart, handleRemoveCart}) {
     const {product, image,  name, price, qnt, countInStock} = productCart;
-    
+    const [quantity, setQnt] = useState(qnt);
+    const dispatch = useDispatch();
+    const handleDecreaseQnt = () => {
+        quantity > 1 && setQnt(quantity - 1);
+       
+      }
+     const handleIncreaseQnt= () => {
+        setQnt(quantity + 1);
+      }
+
+      useEffect(() => {
+        dispatch(addToCart(product, quantity))
+      }, [quantity]);
+      
   return (
     <div id={product} className='cart-item'>
     <div className='cart-item__img'>
@@ -17,7 +34,17 @@ function CartItem({productCart, handleRemoveCart}) {
         <div className='cart-item__price'>${price}</div>
         <div className='cart-item__count'>In Stock: {countInStock}</div>
         <div className='cart-item__handle'>
-            <span className='cart-item__qnt'>Number: {qnt}</span>
+            <Button
+            onClick={handleDecreaseQnt}
+          >
+            <RemoveIcon />
+          </Button>
+          <span className="cart-item__qnt">{quantity}</span>
+          <Button
+            onClick={handleIncreaseQnt}
+          >
+            <AddIcon />
+          </Button>
         </div>
     </div>
     <Button className='cart-item__rm' onClick={handleRemoveCart}>
